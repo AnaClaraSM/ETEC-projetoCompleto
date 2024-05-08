89b93e9faad239672774aef049b522d66b89f299
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-05-08 17:42:42 -0300

    inicio fAcad

---
+++ Controller/FomacaoAcadController.php
@@ -0,0 +1,18 @@
+<?php
+
+    //Se a sessão não tiver sido iniciada
+    if (!isset ($_SESSION)) {
+        //Cria sessão
+        session_start();
+    }
+
+    //Criação da Classe FormacaoAcadController
+    class FormacaoAcadController {
+
+        //Método Inserir
+
+        //Método Remover
+
+        //Método Gerar Lista
+    }
+?>
