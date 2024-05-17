4b77ec76c560cb1bd6a667663f5c288504909e91
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-05-16 21:23:42 -0300

    admin view (interface) finished

--- View/ADMListarCadastrados.php
+++ View/ADMListarCadastrados.php
@@ -26,6 +26,30 @@
         <h1 class="w3-text-white w3-panel w3-cyan w3-round-large">Lista de Usuários Cadastrados no Sistema</h1>
     </header>
     <!-- Fim Cabeçalho -->
-    <!-- Tab -->
+    <!-- Tabela de Usuários Cadastrados -->
+    <div class="w3-padding-128 w3-content w3-text-grey">
+        <div class="w3-container">
+            <table class="w3-table-all w3-centered">
+                <thead>
+                    <tr class="w3-center w3-blue">
+                        <th>Código</th>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <!-- Fim Tabela de Usuários Cadastrados -->
+    <!-- Botão Voltar-->
+    <div class="w3-padding-128 w3-content w3-text-grey">
+        <form action="../Controller/Navegacao.php" method="post" class="w3-container w3-light-grey w3-text-blue w3-margin w3-center" style="width: 30%">
+            <div class="w3-row w3-section">
+                <div>
+                    <button name="btnVoltar" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;">Voltar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- Fim Botão Voltar-->
 </body>
 </html>
